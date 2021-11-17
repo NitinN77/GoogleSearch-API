7d33162ef23d26132b557510f1d732f68dd4e112
VoidlessVoid7 <voidlessvoid7@gmail.com>
2021-11-17 13:19:06 +0530

    css target fixes

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ def fetch():
     uClient.close()
 
     page_soup = Soup(page_html, "html.parser")
-    titles = page_soup.findAll("h3", {"class": "ipQwMb ekueJc RD0gLb"})
+    titles = page_soup.findAll("a", {"class": "DY5T1d RZIKme"})
     images = page_soup.findAll("img", {"class": "tvs3Id QwxBBf"})
     links = page_soup.findAll("a", {"class": "VDXfz"})
     ret = []
@@ -75,11 +75,11 @@ def fetchrepo():
 
         page_soup = Soup(page_html, "html.parser")
 
-        title = page_soup.findAll("a", {"data-pjax": "#js-repo-pjax-container"})
+        title = page_soup.findAll("a", {"data-pjax": "#repo-content-pjax-container"})
         title = title[0].text
         about = page_soup.findAll("p", {"class": "f4 mt-3"})
         about = about[0].text.strip()
-        lang = page_soup.findAll("span", {"class": "color-text-primary text-bold mr-1"})
+        lang = page_soup.findAll("span", {"class": "color-fg-default text-bold mr-1"})
         lang = lang[0].text
         star_count = page_soup.findAll("a", {"class": "social-count js-social-count"})
         star_count = int(star_count[0].text.strip())
@@ -108,7 +108,7 @@ def fetchblogs():
         ret = {}
         ret['title'] = titles[i].text
         ret['link'] = 'https://medium.com' + titles[i]['href']
-        ret['img'] = page_soup.findAll("img", {"class":"af io ky"})[i]['src']
+        ret['img'] = page_soup.findAll("img", {"class":"af io kw"})[i]['src']
         ret['readtime'] = page_soup.findAll("p", {"class":"be b bf bg hf"})[i].findAll("span")[0].text
         ret_arr.append(ret)
     return jsonify(ret_arr)
